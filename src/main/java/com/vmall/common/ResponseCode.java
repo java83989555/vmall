c03f225f647620c3bb6418c53b69311040230021
wei.gao <wei.gao@lepu.im>
2017-06-18 16:45:03 +0800

    commit pm 4:45

---
+++ src/main/java/com/vmall/common/ResponseCode.java
@@ -0,0 +1,28 @@
+package com.vmall.common;
+
+/**
+ * Created by GV
+ * DATE:2017/6/18
+ * TIME:下午3:57
+ */
+public enum ResponseCode {
+    SUCCESS(0, "SUCCESS"),
+    ERROR(-1, "ERROR"),
+    NEED_LONGIN(10,"NEED_LOGIN"),
+    ILLEGAL_ARGUMENT(2,"ILLEGAL_ARGUMENT");
+    private final Integer code;
+    private final String desc;
+
+    ResponseCode(Integer code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
