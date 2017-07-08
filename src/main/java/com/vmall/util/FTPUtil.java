9870ed3634c9596cbd44553e9e0f5bcd6f5f7f74
wei.gao <wei.gao@lepu.im>
2017-07-08 20:34:58 +0800

    deploy on linux update

--- src/main/java/com/vmall/util/FTPUtil.java
+++ src/main/java/com/vmall/util/FTPUtil.java
@@ -31,7 +31,7 @@ public class FTPUtil {
     public static boolean uploadFile(List<File> fileList) throws IOException {
         FTPUtil ftpUtil = new FTPUtil(ftpIp, 21, ftpUser, ftpPass);
         logger.info("开始连接ftp服务器");
-        boolean result = ftpUtil.uploadFile("public", fileList);
+        boolean result = ftpUtil.uploadFile("image", fileList);
         logger.info("开始连接ftp服务器,结束上传,上传结果:{}", result);
         return result;
     }
